0c85acf0ec455919e4a635c0f25041708c1d5e70
Mukul Singh <mukulkumar.singh@bacancy.com>
2022-12-19 20:01:12 +0530

    Riverpod part 1 - completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,75 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
-  // * For more info on error handling, see:
-  // * https://docs.flutter.dev/testing/errors
-  await runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    // turn off the # in the URLs on the web
-    GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
-    // * Entry point of the app
-    runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
 
-    // * This code will present some error UI if any uncaught exception happens
-    FlutterError.onError = (FlutterErrorDetails details) {
-      FlutterError.presentError(details);
-    };
-    ErrorWidget.builder = (FlutterErrorDetails details) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('An error occurred'.hardcoded),
-        ),
-        body: Center(child: Text(details.toString())),
-      );
-    };
-  }, (Object error, StackTrace stack) {
-    // * Log any errors to console
+  //* turn off the '#' in the URLs on the web
+  GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+
+  //* Error handler
+  registerErrorHandlers();
+  // * Entry point of the app
+  runApp(const ProviderScope(child: MyApp()));
+
+  // // * For more info on error handling, see:
+  // // * https://docs.flutter.dev/testing/errors
+  // await runZonedGuarded(() async {
+  //   WidgetsFlutterBinding.ensureInitialized();
+  //   // turn off the # in the URLs on the web
+  //   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+
+  //   registerErrorHandlers();
+  //   // * Entry point of the app
+  //   runApp(const MyApp());
+
+  //   // * This code will present some error UI if any uncaught exception happens
+  //   FlutterError.onError = (FlutterErrorDetails details) {
+  //     FlutterError.presentError(details);
+  //   };
+  //   ErrorWidget.builder = (FlutterErrorDetails details) {
+  //     return Scaffold(
+  //       appBar: AppBar(
+  //         backgroundColor: Colors.red,
+  //         title: Text('An error occurred'.hardcoded),
+  //       ),
+  //       body: Center(child: Text(details.toString())),
+  //     );
+  //   };
+  // }, (Object error, StackTrace stack) {
+  //   // * Log any errors to console
+  //   debugPrint(error.toString());
+  // });
+}
+
+void registerErrorHandlers() {
+  //* shows some error UI if anyuncaught exception happens
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint(details.toString());
+  };
+
+  //* Handle errors from underlying platforms IOS/Andorid
+
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
     debugPrint(error.toString());
-  });
+    return true;
+  };
+
+  //* Show some UI error when any widget in the app fails to build
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('An error occurred'.hardcoded),
+      ),
+      body: Center(child: Text(details.toString())),
+    );
+  };
 }
--- lib/src/features/products/data/fake_product_repository.dart
+++ lib/src/features/products/data/fake_product_repository.dart
@@ -1,11 +1,9 @@
 import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeProductRepository {
-  //Singleton Pattern for Dependecy Injection (Temporary)
-  FakeProductRepository._(); // stops from creating multiple instances of this class
-  static FakeProductRepository instance = FakeProductRepository._();
-
   final List<Product> _products = kTestProducts;
   List<Product> getProductsList() {
     return _products;
@@ -16,11 +14,14 @@ class FakeProductRepository {
   }
 
   Future<List<Product>> fetchProductsList() async {
+    await Future.delayed(const Duration(seconds: 2));
+
     return Future.value(_products);
   }
 
-  Stream<List<Product>> watchProductsList() {
-    return Stream.value(_products);
+  Stream<List<Product>> watchProductsList() async* {
+    await Future.delayed(const Duration(seconds: 2));
+    yield _products;
   }
 
   Stream<Product?> watchProduct(String id) {
@@ -29,3 +30,33 @@ class FakeProductRepository {
     );
   }
 }
+
+final productsRepositoryProvider = Provider<FakeProductRepository>(
+  (ref) {
+    return FakeProductRepository();
+  },
+);
+
+final productListStreamProvider = StreamProvider.autoDispose<List<Product>>(
+  (ref) {
+    debugPrint('created productListStreamProvider');
+    final productRepository = ref.watch(productsRepositoryProvider);
+    return productRepository.watchProductsList();
+  },
+); //Stream Provider
+
+//* Stream and Future providers defintion is same just differs in working
+final productListFutureProvider = FutureProvider.autoDispose<List<Product>>(
+  (ref) {
+    final productRepository = ref.watch(productsRepositoryProvider);
+    return productRepository.fetchProductsList();
+  },
+); // Future Provider
+
+final productProvider = StreamProvider.autoDispose.family<Product?, String>(
+  (ref, id) {
+    debugPrint('created product provider with id = $id');
+    final productRepository = ref.watch(productsRepositoryProvider);
+    return productRepository.watchProduct(id);
+  },
+);// Family Provider used for passing arguments in streams and similarly for futures
